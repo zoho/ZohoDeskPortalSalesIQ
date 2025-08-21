cdee2a29324e120dc3ded41995a67025b056d045
Wasim2708 <wasim.27a@gmail.com>
2025-08-21 12:07:28 +0530

    update to v4.4.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "4.2.0"
+let version = "4.4.0"
 let package = Package(
     name: "ZohoDeskPortalSalesIQ",
     platforms: [
@@ -16,12 +16,12 @@ let package = Package(
             targets: ["ZohoDeskPortalSalesIQ", "Mobilisten", "ZohoDeskPortalSalesIQPackage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: "4.2.0")
+        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: "4.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZohoDeskPortalSalesIQ", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalSalesIQ/\(version)/ZohoDeskPortalSalesIQ.zip", checksum: "b311c3c0fd05045c7bf9e28a7ed6c8d26cb96b5a5b1975c77005282c87588165"),
+        .binaryTarget(name: "ZohoDeskPortalSalesIQ", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalSalesIQ/\(version)/ZohoDeskPortalSalesIQ.zip", checksum: "8726d240e14212e7d1be3ba9c6fd0163513034e13059b4c233d37b725b3d67e2"),
         .binaryTarget(name: "Mobilisten", url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v9.2.6/Mobilisten.zip", checksum: "65129b82119e0a5b67f61544a1474b697f94c5aa75e2ac71bc4ce3fae9e2f506"),
         .target(
             name: "ZohoDeskPortalSalesIQPackage",
