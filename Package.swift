8773487c942cdb626a02de5bb6daa251559aa28e
Vinoth N <vinoth.nv@zohocorp.com>
2026-03-31 17:53:43 +0530

    4.5.8

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = Version("4.4.3")
+let version = Version("4.5.8")
 let package = Package(
     name: "ZohoDeskPortalSalesIQ",
     platforms: [
@@ -13,20 +13,21 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZohoDeskPortalSalesIQ",
-            targets: ["ZohoDeskPortalSalesIQ", "Mobilisten", "ZohoDeskPortalSalesIQPackage"]),
+            targets: ["ZohoDeskPortalSalesIQ", "ZohoDeskPortalSalesIQPackage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: version)
+        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: version),
+        .package(url: "https://github.com/zoho/SalesIQ-Mobilisten-iOS-SP.git", exact: "10.1.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZohoDeskPortalSalesIQ", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalSalesIQ/\(version.description)/ZohoDeskPortalSalesIQ.zip", checksum: "ed45f1c9f242c08089dfe61d7dbc63e6b31ca05a57d3ba977760a8fa8e258279"),
-        .binaryTarget(name: "Mobilisten", url: "https://github.com/zoho/SalesIQ-Mobilisten-iOS/releases/download/v10.1.2/Mobilisten.zip", checksum: "d8e6a5beb18c7ecabe57bc319b915a43c7479efc42cd5720716f81f968513b1a"),
+        .binaryTarget(name: "ZohoDeskPortalSalesIQ", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalSalesIQ/\(version.description)/ZohoDeskPortalSalesIQ.zip", checksum: "5cc0a84097455dc90a858cb4434033da3b724c691593fcf97a0b471b5baa5940"),
         .target(
             name: "ZohoDeskPortalSalesIQPackage",
             dependencies: [
-                .product(name: "ZohoDeskPortalConfiguration", package: "ZohoDeskPortalConfiguration")
+                .product(name: "ZohoDeskPortalConfiguration", package: "ZohoDeskPortalConfiguration"),
+                .product(name: "Mobilisten", package: "SalesIQ-Mobilisten-iOS-SP")
             ]
         )
     ]
